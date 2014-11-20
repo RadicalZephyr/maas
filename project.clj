14e0bf78ce47359f0342a896a66d1b922a0613b2
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-20 18:42:44 -0500

    Fix up the project.clj file

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject maas "0.1.0-SNAPSHOT"
-  :description "FIXME Pallet project for maas"
+  :description "Minecraft as a Service"
+  :url "https://github.com/RadicalZephyr/maas"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.palletops/pallet "0.8.0-RC.9"]
                  [com.palletops/pallet-jclouds "1.7.3"]
@@ -14,6 +17,9 @@
                   :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.jclouds.driver/jclouds-sshj "1.7.2"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks   [environ.leiningen.hooks]
   :profiles {:dev
              {:dependencies
               [[com.palletops/pallet "0.8.0-RC.9"
@@ -22,7 +28,8 @@
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
              :leiningen/reply
              {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.2"]]
-              :exclusions [commons-logging]}}
+              :exclusions [commons-logging]}
+             :production {:env {:production true}}}
   :local-repo-classpath true
   :repositories
   {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
